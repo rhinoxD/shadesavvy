b8ba6199b39cf56712c008abd837278b140517ec
rhinoxD <coolshiv0721@gmail.com>
2021-10-24 09:41:01 +0530

    Add PaletteMetaForm and Dialog

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PaletteMetaForm from './PaletteMetaForm';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import {
@@ -51,20 +52,13 @@ class PaletteFormNav extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-    ValidatorForm.addValidationRule('isPaletteNameUnique', (value) =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value,
     });
   }
   render() {
-    const { classes, open } = this.props;
+    const { classes, open, palettes, handleSubmit } = this.props;
     const { newPaletteName } = this.state;
     return (
       <div className={classes.root}>
@@ -90,21 +84,7 @@ class PaletteFormNav extends Component {
             </Typography>
           </Toolbar>
           <div className={classes.navBtns}>
-            <ValidatorForm
-              onSubmit={() => this.props.handleSubmit(newPaletteName)}
-            >
-              <TextValidator
-                label="Palette Name"
-                value={this.state.newPaletteName}
-                name="newPaletteName"
-                onChange={this.handleChange}
-                validators={['required', 'isPaletteNameUnique']}
-                errorMessages={['Enter Palette Name', 'Name already used!']}
-              />
-              <Button variant="contained" color="primary" type="submit">
-                Save Palette
-              </Button>
-            </ValidatorForm>
+            <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
             <Link to="/">
               <Button variant="contained" color="secondary">
                 Go Back
